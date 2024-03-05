18b6b5e5f312729d149e8258c7437b81ae09dd3e
Xusniddin <kursantmarufov@gmail.com>
2024-03-05 18:09:34 +0500

    bot yaratildi!

--- main.py
+++ main.py
@@ -11,3 +11,22 @@
 #     book1 = Book(*book)
 # print(book1.author, book1.year)
 # -----------------------------------------------------------------------------------
+#2 Telegrambot
+# from telebot import TeleBot
+# from telebot.types import Message
+# bot = TeleBot('7106668865:AAGb7YhiIZcG7nV5xaawapJNKy7nkJGjEk0')
+#
+# @bot.message_handler(commands=['start'])
+# def start(message:Message):
+#     chat_id = message.chat.id
+#     first_name = message.from_user.first_name
+#     print(chat_id)
+#     bot.send_message(chat_id=chat_id, text=f"Assalomu aleykum {first_name}")
+#
+# @bot.message_handler(content_types=['text','photo','video','animation'])
+# def return_to_text(message:Message):
+#     chat_id = message.chat.id
+#     bot.copy_message(-1002118445907, chat_id,message.message_id)
+#
+#
+# bot.polling()
